35a130a4887aa6e3bdb9e056017148ac89ab6f42
Dito Cahya Pratama <ditocahyapratama717@gmail.com>
2020-07-20 22:02:40 +0700

    Adding Export Excel

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,10 +1,17 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Bar } from 'react-chartjs-2'
-import {ReactJsonExcel} from 'react-json-excel'
+import {
+  JsonToExcel
+} from 'react-json-excel';
+import { Button } from 'antd';
 import data from '../data/data.json'
 function BarChart() {
-  const labels = data.dt.map(val=>val.label)
-  const dataVal = data.dt.map(val=>val.value)
+  const labels = data.dt.map(val => val.label)
+  const dataVal = data.dt.map(val => val.value)
+  const fields = {
+    "label": "label",
+    "value": "value"
+  }
   const [dataChart, setDataChart] = useState({
     labels: labels,
     datasets: [
@@ -18,7 +25,11 @@ function BarChart() {
   return (
     <>
       <h3>BarChart Example</h3>
-
+      {/* <Button type="primary"> */}
+        <JsonToExcel data={data.dt}
+          filename="test"
+          fields={fields} />
+      {/* </Button> */}
       <Bar
         data={dataChart}
       />
