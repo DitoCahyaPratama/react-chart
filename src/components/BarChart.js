fdac522bd453f60f181a156b1043b6682870851d
Dito Cahya Pratama <ditocahyapratama717@gmail.com>
2020-07-27 23:10:01 +0700

    Update export library to file-saver and exceljs

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -23,14 +23,42 @@ function BarChart() {
       },
     ]
   })
+  const saveAsExcel = async () => {
+    const wb = new ExcelJS.Workbook()
+    // create new sheet with pageSetup settings for A4 - landscape
+    const ws = wb.addWorksheet('sheet', {
+      pageSetup:{paperSize: 9, orientation: 'landscape'}
+    })
+
+    // adjust pageSetup settings afterwards
+    ws.pageSetup.margins = {
+      left: 0.7, right: 0.7,
+      top: 0.75, bottom: 0.75,
+      header: 0.3, footer: 0.3
+    };
+
+    
+    console.log(data)
+    
+    const title = ws.addRow(['Nama', 'Jumlah'])
+    title.font = { bold: true }
+
+    data.dt.map((val, key) => {
+      ws.addRow([val.label,val.value])
+    })
+
+    const buf = await wb.xlsx.writeBuffer()
+    saveAs(new Blob([buf]), 'abc.xlsx')
+  }
   return (
     <>
       <h3>BarChart Example</h3>
-      {/* <Button type="primary"> */}
-        <JsonToExcel data={data.dt}
+      <Button type="primary" onClick={() => saveAsExcel()}>
+        {/* <JsonToExcel data={data.dt}
           filename="test"
-          fields={fields} />
-      {/* </Button> */}
+          fields={fields} /> */}
+          test
+      </Button>
       <Bar
         data={dataChart}
       />
